17e8e085b23a5f13d766b2305f67674a60f297d4
Samuel Macêdo <107008342+SamuelRuan2@users.noreply.github.com>
2023-03-03 19:48:40 -0300

    Projeto base
    arquivos iniciais

---
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/com/biblioteca_raimundo/biblioteca_raimundo/Biblioteca_Raimundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.biblioteca_raimundo.biblioteca_raimundo;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Biblioteca_Raimundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
