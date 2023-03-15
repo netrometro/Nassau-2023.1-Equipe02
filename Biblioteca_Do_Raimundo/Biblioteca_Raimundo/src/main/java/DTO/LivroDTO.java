820d79328cb78076edcb44db200bb646f6a2ee83
Gabriel Lima <gabbriel.llima7@gmail.com>
2023-03-15 17:03:12 -0300

    encapsulamento

--- Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DTO/LivroDTO.java
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DTO/LivroDTO.java
@@ -10,10 +10,35 @@ public class LivroDTO {
     private String nome_livro, autor_livro, secao_livro;
     private int id_livro;
     
-    public String getNome_Livro() {
+    //Encapsulamento  livro
+    public String getNome_livro() {
         return nome_livro;
-   }
+    }
     public void setNome_livro(String nome_livro) {
         this.nome_livro = nome_livro;
-}
+    }
+    
+    //Encapsulamento  autor
+    public String getAutor_livro() {
+        return autor_livro;
+    }
+    public void setAutor_livro(String autor_livro) {
+        this.autor_livro = autor_livro;
+    }
+    
+    //Encapsulamento  secao
+    public String getSecao_livro() {
+        return secao_livro;
+    }
+    public void setSecao_livro(String secao_livro) {
+        this.secao_livro = secao_livro;
+    }
+    
+    //Encapsulamento  id
+    public int getId_livro() {
+        return id_livro;
+    }
+    public void setId_livro(int id_livro) {
+        this.id_livro = id_livro;
+    }
 }
