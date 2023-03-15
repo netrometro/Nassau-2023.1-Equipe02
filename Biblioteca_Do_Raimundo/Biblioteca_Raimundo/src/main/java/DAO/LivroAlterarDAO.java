e964d69b554bb0a0d4bfdaae9d3fc04e613299fe
Gabriel Lima <gabbriel.llima7@gmail.com>
2023-03-15 17:21:24 -0300

    ação de alterar livros

--- Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/LivroAlterarDAO.java
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/LivroAlterarDAO.java
@@ -2,10 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package DAO;
+import DTO.LivroDTO;
+import DataBase.TratamentoBdDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
- * @author gabri
+ * @author gabriel
  */
 public class LivroAlterarDAO {
+    Connection conn;
+    PreparedStatement pstm;
+    ResultSet rs;
+    
+    public void alterarLivro(LivroDTO objlivroDTO) {
+        String sql = "Update tabelaLivros set nomeL =?, autorL =?, secaoL =? where idL =?";
+        
+        conn = new TratamentoBdDAO().InsertLivroBd(sql, sql, sql);
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            
+            pstm.setString(1, objlivroDTO.getNome_livro()); //1 -> =?
+            pstm.setString(2, objlivroDTO.getAutor_livro()); //2 -> =?
+            pstm.setString(3, objlivroDTO.getSecao_livro()); //3 -> =?
+            pstm.setInt(4, objlivroDTO.getId_livro()); //4 -> =?
+            
+            pstm.execute();
+            pstm.close();
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "LivroAlterarDAO: " + erro);
+        }
+        
+    }
     
 }
