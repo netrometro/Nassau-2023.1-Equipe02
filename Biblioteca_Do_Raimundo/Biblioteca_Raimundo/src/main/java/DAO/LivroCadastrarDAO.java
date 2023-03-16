479fe794f7cef442c085d18c364e850e92609862
Gabriel Lima <gabbriel.llima7@gmail.com>
2023-03-16 00:16:35 -0300

    samuel - movi para arquivo certo

--- Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/LivroCadastrarDAO.java
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/LivroCadastrarDAO.java
@@ -3,11 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-    //Samuel passar para ca codigo de insert
+import DTO.LivroDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author gabri
  */
 public class LivroCadastrarDAO {
     
+    Connection conn;
+    PreparedStatement pstm;
+    
+    public void cadastrarLivro(LivroDTO objlivroDTO) {
+    
+        String sql = "INSERT INTO tabelaLivros(nomeL,autorL,secaoL) VALUES(?,?,?)";
+
+            conn = new ConexaoDAO().ConectaBD();
+
+            try {
+                pstm = conn.prepareStatement(sql);
+                
+                pstm.setString(1, objlivroDTO.getNome_livro());
+                pstm.setString(2,objlivroDTO.getAutor_livro());
+                pstm.setString(3,objlivroDTO.getSecao_livro());
+                pstm.setInt(4, objlivroDTO.getId_livro());
+
+                pstm.execute();
+                pstm.close();
+
+            System.out.println("Gravado!");
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+            JOptionPane.showMessageDialog(null, "LivroAlterarDAO: " + e);
+        }
+    }
 }
