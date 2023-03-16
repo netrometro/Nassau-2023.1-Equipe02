0362eb08259b502cc7c546931618057341905cc5
Gabriel Lima <gabbriel.llima7@gmail.com>
2023-03-16 19:36:59 -0300

    correção para um arquivo só os metodos

--- Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/TratamentoDAO.java
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/DAO/TratamentoDAO.java
@@ -4,6 +4,7 @@
  */
 package DAO;
 
+import DTO.LivroDTO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,21 +16,17 @@ import javax.swing.JOptionPane;
  *
  * @author gabri
  */
-public class LivroCadastrarDAO {
-    
-    Connection conn;
+public class TratamentoDAO {
     PreparedStatement pstm;
+    Connection conn = new ConexaoDAO().ConectaBD();
     
     public void InsertLivroBd(String nome, String autor, String seccao) {
         try {
 
             try {
-                conn = DriverManager.getConnection(
-             "jdbc:sqlite:db/dbBiblioteca.db");
-                
                 Statement statement = conn.createStatement();
-                
                 statement.execute("CREATE TABLE IF NOT EXISTS tabelaLivros(nomeL VARCHAR, autorL VARCHAR, secaoL VARCHAR)");
+                
             } catch (SQLException e1) {
                 JOptionPane.showMessageDialog(null, "LivroCadastroDAO. Conexão: " + e1);
             }
@@ -50,6 +47,27 @@ public class LivroCadastrarDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
+    }
+    //METODO DE ALTERAR LIVRO
+    public void alterarLivro(LivroDTO objlivroDTO) {
+        String sql = "Update tabelaLivros set nomeL =?, autorL =?, secaoL =? where idL =?";
+        
+        conn = new ConexaoDAO().ConectaBD();
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            
+            pstm.setString(1, objlivroDTO.getNome_livro()); //1 -> =?
+            pstm.setString(2, objlivroDTO.getAutor_livro()); //2 -> =?
+            pstm.setString(3, objlivroDTO.getSecao_livro()); //3 -> =?
+            pstm.setInt(4, objlivroDTO.getId_livro()); //4 -> =?
+            
+            pstm.execute();
+            pstm.close();
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "LivroAlterarDAO: " + erro);
+        }
+        
     }
 }
