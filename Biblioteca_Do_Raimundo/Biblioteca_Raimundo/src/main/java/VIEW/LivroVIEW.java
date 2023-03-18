194b471fc94940039728ca4dd16ce707e136e8a6
samuelruan2 <samuelruan2@gmail.com>
2023-03-17 21:11:33 -0300

    ATT

--- Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/VIEW/LivroVIEW.java
+++ Biblioteca_Do_Raimundo/Biblioteca_Raimundo/src/main/java/VIEW/LivroVIEW.java
@@ -1,6 +1,6 @@
 package VIEW;
 
-import DAO.*;
+import DAO.TratamentoDAO;
 import java.sql.*;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -314,9 +314,9 @@ public class LivroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnAlterarActionPerformed
 
     private void btnSairVIEW2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairVIEW2ActionPerformed
-        this.dispose();
-        DashBoardVIEW tela = new DashBoardVIEW();
-        tela.setVisible(true);
+//        this.dispose();
+//        DashBoardVIEW tela = new DashBoardVIEW();
+//        tela.setVisible(true);
     }//GEN-LAST:event_btnSairVIEW2ActionPerformed
 
     /**
